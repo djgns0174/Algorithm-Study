cc5d6339bebeb75aa24f72f32cf10252418206ae
Jeong-Je <jeongjeson656@gmail.com>
2025-05-26 18:55:48 +0900

    [jeongje] 20166 - 문자열 지옥에 빠진 호석 (2점 / 16점)

---
+++ jeongje/9_week/20166_문자열 지옥에 빠진 호석 (부분 성공).cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+static char arr[11][11];
+
+int N, M, K;
+map<string, int> m;
+
+void DFS(int i, int j, int depth, string s);
+
+int di[8] = {-1 ,1, 0, 0, 1, 1, -1, -1};
+int dj[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+
+int main(){
+    cin >> N >> M >> K;
+   
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=M;j++){
+            cin >> arr[i][j];
+        }
+    }
+   
+    for(int i=0;i<K;i++){
+        string str;
+        cin >> str;
+       
+        m.insert({str, 0});
+    }
+   
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=M;j++){
+            DFS(i, j, 1, "");
+        }
+    }
+   
+   
+    // 정답 출력
+    for(auto iter=m.begin(); iter != m.end(); iter++){
+        cout << iter->second << "\n";
+    }
+   
+    return 0;
+}
+
+void DFS(int i, int j, int depth, string s) {
+    if(depth > 5) return;
+   
+    s += arr[i][j];
+    if(m.find(s) != m.end()) {
+        m[s]++;
+    }
+   
+    for(int d=0;d<8;d++){
+        int next_i = i + di[d];
+        int next_j = j + dj[d];
+       
+        // 환형 이동 처리
+        if(next_i == 0) next_i = N;
+        if(next_i == N+1) next_i = 1;
+        if(next_j == 0) next_j = M;
+        if(next_j == M+1) next_j = 1;
+       
+        DFS(next_i, next_j, depth + 1, s);
+    }
+}
